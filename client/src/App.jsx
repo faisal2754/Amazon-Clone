4629fbcd674468f888a016038723e721899c23b7
Faisal <saleemfs27@gmail.com>
2021-08-01 17:26:39 +0200

    Setup React Router & additional components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
+import Checkout from './components/Checkout'
+import Header from './components/Header'
+import Home from './components/Home'
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>bruh</h1>
+    <div className="app">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
