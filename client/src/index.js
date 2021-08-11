59df022717d298b7d326d144f4e57f8e51262130
Faisal <saleemfs27@gmail.com>
2021-08-11 12:32:29 +0200

    Formatting

--- client/src/index.js
+++ client/src/index.js
@@ -6,10 +6,10 @@ import { StateProvider } from './state/StateProvider'
 import reducer, { initialState } from './state/reducer'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <App />
-    </StateProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+   <React.StrictMode>
+      <StateProvider initialState={initialState} reducer={reducer}>
+         <App />
+      </StateProvider>
+   </React.StrictMode>,
+   document.getElementById('root')
 )
