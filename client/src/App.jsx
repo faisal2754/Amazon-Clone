0e745b8d997799d4abaa0a389080b08c243eae8d
Faisal <saleemfs27@gmail.com>
2021-08-11 12:42:10 +0200

    Removed unused import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './App.css'
 import Checkout from './components/Checkout'
 import Header from './components/Header'
 import Home from './components/Home'
